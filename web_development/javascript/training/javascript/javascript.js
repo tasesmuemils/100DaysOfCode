7a2152046722bd3516771f24bd0c205b9e1d07ce
tasesmuemils <emils.bisenieks93@gmail.com>
2018-04-04 23:31:51 +0300

    04.04 Training

--- web_development/javascript/training/javascript/javascript.js
+++ web_development/javascript/training/javascript/javascript.js
@@ -1,15 +1,16 @@
-
+//Change background
 var bgContainer = document.getElementById('bgColorChange');
 
 function changeBackgroundColor() {
   bgContainer.className = 'colorChange';
 }
 
-
 var bgButton = document.getElementById('backgroundButton');
 bgButton.addEventListener('click', changeBackgroundColor);
+//
 
 
+//Change sentence style
 function changeTextStyle() {
   var changeText = document.getElementById('textChange');
   changeText.className = 'changeTextLook';
@@ -17,10 +18,9 @@ function changeTextStyle() {
 
 var bgButton = document.getElementById('textButton');
 bgButton.addEventListener('click', changeTextStyle);
+//
 
-
-
-
+//Show values of the form when holding on button
 var firstName = document.getElementById('name');
 var lastName = document.getElementById('surname')
 function getNameAndUsername() {
@@ -32,3 +32,35 @@ function getNameAndUsername() {
 
 var getValues = document.getElementById('getValue');
 getValues.addEventListener('mousedown', getNameAndUsername);
+
+//all values of a tag
+var aTag = document.getElementById('w3r');
+
+function getValuesFromATag() {
+  var aTagLink = aTag.getAttribute('href');
+  var aTagHrefLang = aTag.getAttribute('hreflang');
+  var aTagRel = aTag.getAttribute('rel');
+  var aTagTarget = aTag.getAttribute('target');
+  var resultsOfATag = document.getElementById('scriptResultSecond');
+  resultsOfATag.innerHTML = 'Link: ' + aTagLink + '<br>Language: ' + aTagHrefLang +
+  '<br>Rel:' + aTagRel + '<br>Target: ' + aTagTarget;
+}
+
+var getValuesATag = document.getElementById('ValuesButton');
+getValuesATag.addEventListener('click', getValuesFromATag);
+
+//insert Row
+
+var newRowPosition = document.getElementsByTagName('table')[0];
+
+function newRowFun() {
+  var newRow = document.createElement('tr');
+  var newCell = document.createElement('td');
+  var newCellsText = document.createTextNode('Extra cells');
+  newCell.appendChild(newCellsText);
+  newRow.appendChild(newCell);
+  newRowPosition.appendChild(newRow);
+}
+
+var newRowButton = document.getElementById('insertRow');
+newRowButton.addEventListener('click', newRowFun)
