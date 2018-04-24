9f8b7cac18cef279c3665579b4432b364dfe8d21
tasesmuemils <emils.bisenieks93@gmail.com>
2018-04-24 23:50:51 +0300

    training 01

--- web_development/javascript/training/javascript/javascript.js
+++ web_development/javascript/training/javascript/javascript.js
@@ -1,19 +1,24 @@
 //Change background
-var bgContainer = document.getElementById('bgColorChange');
 
 function changeBackgroundColor() {
-  bgContainer.className = 'colorChange';
+  var bgParent = document.getElementById('bgColorChange');
+  //get h4 from parent container
+  var bgChild = bgParent.getElementsByTagName('h4');
+  //get the first h4 (in this case only one)
+  var setBackgroung = bgChild[0];
+  setBackgroung.style.background = '#790000';
 }
 
 var bgButton = document.getElementById('backgroundButton');
 bgButton.addEventListener('click', changeBackgroundColor);
-//
-
 
 //Change sentence style
 function changeTextStyle() {
   var changeText = document.getElementById('textChange');
-  changeText.className = 'changeTextLook';
+  changeText.style.color = '#7e1111';
+  changeText.style.fontSize = '30px';
+  changeText.style.outlineStyle = 'solid';
+  changeText.style.textDecoration = 'underline';
 }
 
 var bgButton = document.getElementById('textButton');
@@ -21,17 +26,15 @@ bgButton.addEventListener('click', changeTextStyle);
 //
 
 //Show values of the form when holding on button
-var firstName = document.getElementById('name');
-var lastName = document.getElementById('surname')
 function getNameAndUsername() {
-  var fName = firstName.getAttribute('value');
-  var fSurname = lastName.getAttribute('value');
+  var firstName = document.getElementById('name').getAttribute('value');
+  var lastName = document.getElementById('surname').getAttribute('value');
   var results = document.getElementById('scriptResults')
-  results.textContent = 'Your name is' + ' ' + fName + ' and your last name is ' + fSurname;
+  results.textContent = 'Your name is' + ' ' + firstName + ' and your last name is ' + lastName;
 }
 
 var getValues = document.getElementById('getValue');
-getValues.addEventListener('mousedown', getNameAndUsername);
+getValues.addEventListener('click', getNameAndUsername);
 
 //all values of a tag
 var aTag = document.getElementById('w3r');
@@ -42,7 +45,7 @@ function getValuesFromATag() {
   var aTagRel = aTag.getAttribute('rel');
   var aTagTarget = aTag.getAttribute('target');
   var resultsOfATag = document.getElementById('scriptResultSecond');
-  resultsOfATag.innerHTML = 'Link: ' + aTagLink + '<br>Language: ' + aTagHrefLang +
+  resultsOfATag.innerHTML = 'These are all tags from this link: <br>Link: ' + aTagLink + '<br>Language: ' + aTagHrefLang +
   '<br>Rel:' + aTagRel + '<br>Target: ' + aTagTarget;
 }
 
@@ -51,16 +54,44 @@ getValuesATag.addEventListener('click', getValuesFromATag);
 
 //insert Row
 
-var newRowPosition = document.getElementsByTagName('table')[0];
-
 function newRowFun() {
+  var newRowPosition = document.getElementsByTagName('table')[0];
   var newRow = document.createElement('tr');
   var newCell = document.createElement('td');
+  var newCell2 = document.createElement('td');
   var newCellsText = document.createTextNode('Extra cells');
+  var newCellsText2 = document.createTextNode('Extra cells');
   newCell.appendChild(newCellsText);
+  newCell2.appendChild(newCellsText2);
   newRow.appendChild(newCell);
+  newRow.appendChild(newCell2);
   newRowPosition.appendChild(newRow);
 }
 
 var newRowButton = document.getElementById('insertRow');
 newRowButton.addEventListener('click', newRowFun)
+
+//change contetn of specific cell
+function changeCellContent() {
+  //at first lets capture all entered values
+  var rowNumber  = document.getElementById('inputRowNumber').value;
+  var cellNumber  = document.getElementById('inputCellNumber').value;
+  var enteredContent  = document.getElementById('newCellContent').value;
+  //capture table
+  var wholeTable = document.getElementById('myTable2');
+  //capture all table rows
+  var wholeTableRows = wholeTable.getElementsByTagName('tr');
+  for (var i = 0; i < wholeTableRows.length; i++) {
+    if (wholeTableRows[i] === rowNumber) {
+      var wholeTableCells = wholeTableRows.getElementsByTagName('td');
+      for (var z = 0; z < wholeTableCells.length; z++) {
+        if (wholeTableCells[z].length === cellNumber) {
+          wholeTableCells[z].textContent = enteredContent;
+        }
+      }
+    }
+  }
+}
+
+var newCellContentButton = document.getElementById('changeContentOfTheCell');
+newCellContentButton.addEventListener('click', changeCellContent);
