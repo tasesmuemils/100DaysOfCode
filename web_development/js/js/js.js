7a82e071b41e200db228b2854e9cd4f86e8fd129
Emils Bisenieks <emils.bisenieks93@gmail.com>
2018-11-01 15:43:08 +0200

    js update

--- web_development/js/js/js.js
+++ web_development/js/js/js.js
@@ -1,4 +1,4 @@
-//Section1
+//Using a variables to store a number
 var price;
 var quantity;
 var total;
@@ -10,7 +10,7 @@ total = price * quantity;
 var el = document.getElementById("cost");
 el.textContent = '$' + total;
 
-//Section2
+//Using a variable to store a string
 var username;
 var message;
 
@@ -22,3 +22,31 @@ elName.textContent = username;
 var elNote = document.getElementById("note");
 elNote.textContent = message;
 
+//Using quotes inside string
+var title;
+var message;
+
+title = "Emils Special Offers!";
+message = ' \"Double quotes\" !!! 25% off';
+
+var elTitle = document.getElementById("title2");
+elTitle.innerHTML = title;
+var elNote = document.getElementById("note2");
+elNote.innerHTML = message;
+
+//Using a variable to store a boolean
+var inStock;
+var shipping;
+
+inStock = true;
+shipping = false;
+
+//Changing the value of variable
+inStock = false;
+shipping = true;
+
+var elStock = document.getElementById("stock");
+elStock.textContent = inStock;
+
+var elShip = document.getElementById("shipping");
+elShip.textContent = shipping;
