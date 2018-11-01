84b8b3438f68370a7d8a2fd3ae037a93e127f9af
tasesmuemils <tasesmuemils@DESKTOP-HGK6R1Q.localdomain>
2018-11-01 23:32:31 +0200

    Variables refresh

--- web_development/js/js/js.js
+++ web_development/js/js/js.js
@@ -37,7 +37,6 @@ elNote.innerHTML = message;
 //Using a variable to store a boolean
 var inStock;
 var shipping;
-
 inStock = true;
 shipping = false;
 
@@ -47,6 +46,45 @@ shipping = true;
 
 var elStock = document.getElementById("stock");
 elStock.textContent = inStock;
-
 var elShip = document.getElementById("shipping");
 elShip.textContent = shipping;
+
+//Creating an array
+//This way of creating array is called "array literal"
+var colors;
+colors = ['white', 'black', 'custom'];
+/*
+This way of creating array is called "array constructor"
+var colors = new Array (
+    'white',
+    'black',
+    'custom'
+)
+*/
+var elColors =document.getElementById("colors");
+elColors.textContent = colors[0];
+
+//Accessing and changing values in an array
+colors[2] = "beige";
+elColors.textContent = colors[2];
+
+//Using arithmetic operators
+var subtotal = ( 15 + 3 ) * 3;
+var shippingArithmetic = 0.5 * (15 + 3);
+var totatl = subtotal + shippingArithmetic;
+
+var elSub = document.getElementById("subtotal");
+elSub.textContent = subtotal;
+var elShippingArithmetic = document.getElementById("shippingArithmetic");
+elShippingArithmetic.textContent = shippingArithmetic;
+var elTotal = document.getElementById("total");
+elTotal.textContent = total;
+
+//Using string operators
+var greeting = 'Hi ';
+var nameTwo = 'Emils';
+
+var welcomeMessage = greeting + nameTwo + '!';
+
+var elGreet = document.getElementById("greeting");
+elGreet.textContent = welcomeMessage;
