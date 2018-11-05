546278c038b4f622c0a94bfca11a43779ddbce6b
Emils Bisenieks <emils.bisenieks93@gmail.com>
2018-11-05 16:45:53 +0200

    Add files via upload

---
+++ web_development/js/js/fmo_js.js
@@ -0,0 +1,9 @@
+//A basic function
+var msg = 'Sign up to receive our newsletter for 10% off!';
+
+function updateMessage() {
+    var el = document.getElementById("message");
+    el.textContent = msg;
+}
+
+updateMessage();
