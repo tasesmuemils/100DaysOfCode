2decc1c8b24ce8451670a10a96fab1a8fb66f5d5
Emils Bisenieks <emils.bisenieks93@gmail.com>
2018-11-06 16:49:17 +0200

    update js

--- web_development/js/js/fmo_js.js
+++ web_development/js/js/fmo_js.js
@@ -7,3 +7,50 @@ function updateMessage() {
 }
 
 updateMessage();
+
+//Creating objects using literal notation
+var hotel = {
+    name: 'Quay',
+    rooms: 40,
+    booked: 25,
+    checkAvailabilty: function() {
+        return this.rooms - this.booked
+    }
+}
+
+var elName = document.getElementById("hotelName");
+elName.textContent = hotel.name;
+
+var elRooms = document.getElementById("rooms");
+elRooms.textContent = hotel.checkAvailabilty();
+
+//Creating more object literals
+var hotel2 = {
+    name: 'Park',
+    rooms: 120,
+    booked: 77,
+    checkAvailabilty: function () {
+        return this.rooms - this.booked;
+    }
+}
+
+var elName2 = document.getElementById('hotelName2');
+elName2.textContent = hotel2.name;
+
+var elRooms2 = document.getElementById("rooms2");
+elRooms2.textContent = hotel2.checkAvailabilty();
+
+//Creating objects using constructor syntax
+var hotel3 = new Object();
+
+hotel3.name = 'Jandze';
+hotel3.rooms = 156;
+hotel3.booked = 68;
+hotel3.checkAvailabilty = function()  {
+    return hotel3.rooms - hotel3.booked
+}
+
+var elName3 = document.getElementById("hotelName3");
+elName3.textContent = hotel3.name;
+var elRooms3 = document.getElementById("rooms3");
+elRooms3.textContent = hotel3.checkAvailabilty();
