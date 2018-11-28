163f502978879af2880bd0ea4f77a0dae085dd0f
Emils Bisenieks <emils.bisenieks93@gmail.com>
2018-11-28 16:59:19 +0200

    task 3

--- web_development/js_w3resource/js/script.js
+++ web_development/js_w3resource/js/script.js
@@ -10,8 +10,17 @@ var daysNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday
 var day = daysNames[today.getDay()];
 var year = today.getFullYear();
 var hours = today.getHours();
+if (hours < 10) {
+    hours = '0' + hours;
+}
 var minutes = today.getMinutes();
+if (minutes < 10) {
+    minutes = '0' + minutes;
+}
 var seconds = today.getSeconds();
+if (seconds < 10) {
+    seconds = '0' + seconds;
+}
 
 
 var msgDate = document.getElementById('dateSolution');
@@ -20,3 +29,33 @@ msgDate.innerHTML += '<p>Current time is: ' + hours + ':' + minutes + ':' + seco
 
 //Task 2
 //Write a JavaScript program to print the contents of the current window.
+var wContentWidth = window.innerWidth;
+var wContentHeight = window.innerHeight;
+var wContentLocation = window.location;
+
+document.getElementById('windowWidth').textContent = wContentWidth;
+document.getElementById('windowHeight').textContent = wContentHeight;
+document.getElementById('windowLocation').textContent = wContentLocation;
+
+//Tska 3
+/*Write a JavaScript program to get the current date.
+Expected Output : mm-dd-yyyy, mm/dd/yyyy or dd-mm-yyyy, dd/mm/yyyy */
+var today2 = new Date();
+var todays2Date = today2.getDate();
+if (todays2Date < 10) {
+    todays2Date = '0' + todays2Date;
+}
+var todays2Month = today2.getMonth();
+if (todays2Month < 10) {
+    todays2Month = '0' + todays2Month;
+}
+var todays2Year = today2.getFullYear();
+var messageDate1 = todays2Month + '/' + todays2Date + '/' + todays2Year + '<br />';
+messageDate1 += todays2Month + '-' + todays2Date + '-' + todays2Year + '<br />';
+messageDate1 += todays2Date + '-' + todays2Month + '-' + todays2Year + '<br />';
+messageDate1 += todays2Date + '/' + todays2Month + '/' + todays2Year;
+
+document.getElementById('date2Solution').innerHTML = messageDate1;
+
+
+
