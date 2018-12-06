3adccb5529870ddcbdec4ffecf9735d225093fc7
Emils Bisenieks <emils.bisenieks93@gmail.com>
2018-12-06 16:28:32 +0200

    dom js update

--- web_development/js/js/dom_js.js
+++ web_development/js/js/dom_js.js
@@ -1,3 +1,19 @@
 //Selecting individual elements using ID attributes
 var el = document.getElementById('one');
 el.className = 'cool';   
+
+//Selecting elements using CLASS attributes
+var elements2 = document.getElementsByClassName('hot2');
+
+if (elements2.length > 1) {
+    var el2 = elements2.item(2);
+    el2.className = 'cool'; 
+}
+
+//Selecting elements by tag name (1 task)
+var elements3 = document.getElementsByTagName('li');
+
+if (elements3.length > 0) {
+    var el3 = elements3[1];
+    el3.className = 'cool';
+}
