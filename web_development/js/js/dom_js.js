cae1d1904a71e1e8a70c954d8b0fd21cb18bf07d
tasesmuemils <root@DESKTOP-HGK6R1Q.localdomain>
2019-01-07 21:37:26 +0200

    parant, child nodes

--- web_development/js/js/dom_js.js
+++ web_development/js/js/dom_js.js
@@ -17,3 +17,44 @@ if (elements3.length > 0) {
     var el3 = elements3[1];
     el3.className = 'cool';
 }
+
+//Selecting elements using CSS selector
+//querySelector() only returns first value
+var el4 = document.querySelector('li.hot4');
+el4.className = 'cool';
+
+//querySelectorAll() returns NodeList
+//The seconfd matching element (the third list item) is selected and changed
+var el4point2 = document.querySelectorAll('li.hot4');
+el4point2[1].className = 'cool';
+
+//Looping through a NiodeList
+var allElements5 = document.querySelectorAll('li.hot5');
+for (var i=0; i < allElements5.length; i++) {
+    allElements5[i].className = 'cool';
+}
+
+//Previous and next sibling
+//to use theese on Chrome, I have to get rid of whitespaces
+var startItem = document.getElementById('two6');
+var previousItem = startItem.previousSibling;
+var nextItem = startItem.nextSibling;
+
+previousItem.className = 'complete';
+nextItem.className = 'cool';
+
+//First and last child
+//to use theese on Chrome, I have to get rid of whitespaces
+var startItem2 = document.getElementsByTagName('ul')[6];
+var firstItem2 = startItem2.firstChild;
+var lastItem2 = startItem2.lastChild;
+
+firstItem2.setAttribute('class', 'complete');
+lastItem2.setAttribute('class', 'cool');
+
+
+
+
+
+
+
