d683cfe01d399d1972cb5296bd57aec49f53809b
tasesmuemils <root@DESKTOP-HGK6R1Q.localdomain>
2019-01-14 00:13:37 +0200

    event delagation

--- web_development/js/js/events_js.js
+++ web_development/js/js/events_js.js
@@ -23,3 +23,52 @@ function checkUsername2() {
 
 var elUsername2 = document.getElementById('username2');
 elUsername2.addEventListener('blur', checkUsername2, false);
+
+//using parameters with event listeners
+var elUsername3 = document.getElementById('username3');
+var elMsg3= document.getElementById('feedback3');
+
+function checkUsername3(minLength) {
+    if (elUsername3.value.length < 6) {
+        elMsg3.textContent = 'Username must be higher then 6 characters!'
+    } else {
+        elMsg3.textContent = '';
+    }
+}
+
+elUsername3.addEventListener('blur', function() {
+    checkUsername3(6);
+}, false);
+
+//using event delegation
+function getTarget(e) {
+    if(!e) {
+        e = window.event;
+    }
+    return e.target || e.srcElement;
+}
+
+function itemDone(e) {
+    var target, elParent, elGrandparent;
+    target = getTarget(e);
+    elParent = target.parentNode;
+    elGrandparent = target.parentNode.parentNode;
+    elGrandparent.removeChild(elParent);
+    if (e.preventDefault) {
+        e.preventDefault();
+    } else {
+        e.returnValue = false;
+    }
+    
+}
+
+var el = document.getElementById('shoppingList');
+if (el.addEventListener) {
+    el.addEventListener('click', function(e) {
+        itemDone(e);
+    }, false); 
+} else {
+    el.attachEvent('onclick', function(e) {
+        itemDone(e);
+    });
+}
