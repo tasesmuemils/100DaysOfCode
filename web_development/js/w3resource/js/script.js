6e46d8bd1273f37ab43254d364d033e57229dff3
Emils Bisenieks <emils.bisenieks93@gmail.com>
2019-01-21 00:11:05 +0200

    js update

--- web_development/js/w3resource/js/script.js
+++ web_development/js/w3resource/js/script.js
@@ -158,9 +158,21 @@ var randomImage9 = document.getElementById('randomImage9');
 var array9 = ['http://farm4.staticflickr.com/3691/11268502654_f28f05966c_m.jpg',
  'http://farm1.staticflickr.com/33/45336904_1aef569b30_n.jpg',
 'http://farm6.staticflickr.com/5211/5384592886_80a512e2c9.jpg'];
+var taskButton9 = document.getElementById('taskButton9');
+var randNum = document.getElementById('randNum');
 
 
-var imageRandom = Math.floor(Math.random() * 3) + 1;
-var srcChange = randomImage9.src[imageRandom];
+function changeImage() {
+    for (let i = 0; i < array9.length; i++) {
+        var imageRandom = Math.floor(Math.random() * array9.length);
+        if (i === imageRandom) {
+            randomImage9.src = array9[i];
+            array9.splice(i);
+        }
+    }
+}
+
+taskButton9.addEventListener('click', changeImage, false);
+
 
 
