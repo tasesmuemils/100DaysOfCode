a99228d14787fa179ea1e41f7b61435c146b6892
tasesmuemils <root@DESKTOP-HGK6R1Q.localdomain>
2019-01-24 01:13:36 +0200

    ADD GAME

--- web_development/js/product_page/js/script.js
+++ web_development/js/product_page/js/script.js
@@ -65,12 +65,14 @@ var cardPrice = document.getElementsByClassName('cardPrice');
 */
 
 //I use immediately invoked function expression(IIFE) just to wrap code
+//When I got further, I realized that I have to call function again so that means I cant use IIFE
 //This function loops threw array, take all necessary things from array and set content in page
 //Example above was first approach how I managed this loop, before I knew about TEMPLATE STRINGS
 
-(function () {
+var contentProducts = document.getElementById('contentProducts');
+
+function gamesInPage() {
     for (let i = 0; i < games.length; i++) {
-        var contentProducts = document.getElementById('contentProducts');
         var card = document.createElement('div');
         var template = `<img class="cardImage" src="${games[i].src}" alt="">
                     <h3 class="cardTitle">${games[i].title}</h3>
@@ -78,17 +80,61 @@ var cardPrice = document.getElementsByClassName('cardPrice');
         card.className = 'card';
         card.innerHTML = template;
         contentProducts.appendChild(card);
-    }
-}());
+    };
+};
+gamesInPage();
 
 //Form modal
 //Click on button, fill out the form
-var formButton = document.getElementById('formButton');
+var formButton = document.getElementById('formButton'); //open modal
+var formModal = document.getElementById('formModal');
+var closeModalButton = document.getElementById('closeModal'); //close modal
 
-function formModal() {
-    var formModal = document.getElementById('formModal');
+function showModal() {
     formModal.style.display = 'grid';
 }
+formButton.addEventListener('click', showModal, false);
 
-formButton.addEventListener('click', formModal, false);
+function closeModal() {
+    formModal.style.display = 'none';
+}
+closeModalButton.addEventListener('click', closeModal, false);
+
+var productID = document.getElementById('productID');
+var consoleType = document.getElementById('consoleType');
+var productName = document.getElementById('productName');
+var productImg = document.getElementById('productImg');
+var productPrice = document.getElementById('productPrice');
+var productDescription = document.getElementById('productDescription');
+var addGameButton = document.getElementById('addGameButton');
+
+productID.valueAsNumber = games.length + 1;
+
+function addNewGame() {
+    var productIDIndex = productID.valueAsNumber - 1;
+    var newGame = {
+        id: productID.valueAsNumber,
+        src: productImg.value,
+        title: productName.value,
+        price: productPrice.value
+    };
+    games.push(newGame);
+    
+    (function() {
+        var card = document.createElement('div');
+        var template = `<img class="cardImage" src="${games[productIDIndex].src}" alt="">
+                    <h3 class="cardTitle">${games[productIDIndex].title}</h3>
+                    <p class="cardPrice">${games[productIDIndex].price}</p>`;
+        card.className = 'card';
+        card.innerHTML = template;
+        contentProducts.appendChild(card);
+    }());
+    formModal.style.display = 'none';
+}
+
+
+
+
+addGameButton.addEventListener('click', addNewGame, false);
+
 
