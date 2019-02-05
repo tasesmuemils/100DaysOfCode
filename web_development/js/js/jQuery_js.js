97ac91075ccb76fedf3aefbc75724698a32cb217
Emils Bisenieks <emils.bisenieks93@gmail.com>
2019-02-05 16:23:03 +0200

    js update

--- web_development/js/js/jQuery_js.js
+++ web_development/js/js/jQuery_js.js
@@ -26,16 +26,16 @@ var $listItemText = $('#task2 li').text();
 $('#task2 li').append('<i>' + $listItemText + '</i>');
 
 //CHANGING CONTENT
-$(function() {
+$(function () {
     $('#task3 li:contains("pine")').text('almonds');
-    $('#task3 li.hot').html(function() {
+    $('#task3 li.hot').html(function () {
         return '<em>' + $(this).text() + '</em>';
     });
     $('#task3 li#one').remove();
 })
 
 //ADDING NEW CONTENT
-$(function() {
+$(function () {
     $('#task4 ul').before('<p class="notice">Just update</p>');
     $('#task4 li.hot').prepend('+ ');
     var $newListItem = $('<li><em>gluten-free</em> soy souce</li>');
@@ -43,7 +43,7 @@ $(function() {
 })
 
 //WORKING WITH ATTRIBUTES
-$(function() {
+$(function () {
     $('#task5 li#three').removeClass('hot');
     $('#task5 li.hot').addClass('far');
     $('#task5 li.hot').addClass('fa-heart');
@@ -51,49 +51,160 @@ $(function() {
 })
 
 //CHANGING CSS RULES
-$(function() {
+$(function () {
     var backgroundColor = $('#task6 li').css('background-color');
     $('#task6 ul').append('<p>Color was: ' + backgroundColor + '</p>');
     $('#task6 li').css({
-        'background-color' : '#c5a996',
-        'border' : '2px solid #fff',
-        'color' : '#000',
-        'font-family' : 'Georgia',
-        'padding-left' : '+=75'
+        'background-color': '#c5a996',
+        'border': '2px solid #fff',
+        'color': '#000',
+        'font-family': 'Georgia',
+        'padding-left': '+=75'
     })
 })
 
 //USING .EACH()
-$(function() {
-    $('#task7 li').each(function() {
+$(function () {
+    $('#task7 li').each(function () {
         var ids = this.id;
         $(this).append('<span class="order">' + ' ' + ids + '</span>');
     });
 });
 
 //EVENTS
-$(function() {
+$(function () {
     var ids = '';
     var $listItems = $('#task8 li');
 
-    $listItems.on('mouseover', function() {
+    $listItems.on('mouseover', function () {
         ids = this.id;
         $listItems.children('span').remove();
         $(this).append(' <span class="priority">' + ids + '</span>');
     });
 
-    $listItems.on('mouseout', function() {
+    $listItems.on('mouseout', function () {
         $(this).children('span').remove();
     });
 });
 
 //EVENT OBJECT
-$(function () { 
-    $('#task9 li').on('click', function(e) {
+$(function () {
+    $('#task9 li').on('click', function (e) {
         $('#task9 li span').remove();
         var date = new Date();
         //date.setTime(e.timeStamp); //Dont work on Chrome, but works without
         var clicked = date.toDateString();
         $(this).append('<span class="date"> ' + clicked + ' ' + e.type + ' ' + e.which + '</span>');
     });
+});
+
+//DELEGATING EVENTS
+$(function () {
+    var listItem, itemStatus, eventType;
+
+    $('#task10 ul').on(
+        'click mouseover',
+        ':not(#four)', {
+            status: 'important'
+        },
+        function (e) {
+            listItem = 'Item: ' + e.target.textContent + '<br />';
+            itemStatus = 'Status: ' + e.data.status + '<br />';
+            eventType = 'Event: ' + e.type;
+            $('#scriptResult10').html(listItem + itemStatus + eventType);
+        }
+    );
+});
+
+//BASIC EFFECTS
+$(function () {
+    $('#task11 h2').hide().slideDown(700);
+    var $li = $('#task11 li');
+    $li.hide().each(function (index) {
+        $(this).delay(700 * index).slideDown(500).fadeIn(700);
+    });
+    $li.on('click', function () {
+        $(this).slideUp(300).fadeOut(700);
+    });
+});
+
+//USING ANIMATION
+$(function () {
+    $('#task12 li').on('click', function () {
+        $(this).animate({
+            opacity: 0.0,
+            paddingLeft: '+=80'
+        }, 1100, function () {
+            $(this).remove();
+        });
+    });
+});
+
+//TRAVERSING
+$(function () {
+    var $h2 = $('#task13 h2');
+    $('#task13 ul').hide();
+    $h2.append(' <a>show</a>');
+
+    $h2.on('click', function () {
+        $h2.next().fadeIn(1100).children('.hot').addClass('complete');
+        $h2.find('a').fadeOut(2000);
+    });
+});
+
+//FILTERS IN USE
+var $listItems14 = $('#task14 li');
+$listItems14.filter('.hot:last').removeClass('hot');
+$('#task14 li:not(.hot)').addClass('cool');
+$listItems14.has('em').addClass('complete');
+
+$listItems14.each(function () {
+    var $this = $(this);
+    if ($this.is('.hot')) {
+        $this.prepend('Priority item: ');
+    }
+});
+
+$('li:contains("honey")').append(' (local)');
+
+//USING INDEX NUMBERS
+$(function () {
+    $('#task15 li:lt(2)').removeClass('hot');
+    $('#task15 li').eq(0).addClass('complete');
+    $('#task15 li:gt(2)').addClass('cool');
+});
+
+//WORKING WITH FORMS
+$(function () { 
+    var $newItemButton = $('#newItemButton');
+    var $newItemForm = $('#newItemForm');
+    var $textInput = $('input:text');
+
+    $newItemButton.show();
+    $newItemForm.hide();
+
+    $('#showForm').on('click', function () { 
+        $newItemButton.hide();
+        $newItemForm.show();
+     });
+
+    $newItemForm.on('submit', function (e) { 
+        e.preventDefault();
+        var newText = $('input:text').val();
+        $('#task16 li:last').after('<li>' + newText + '</li>');
+        $newItemForm.hide();
+        $newItemButton.show();
+        $textInput.val('');
+     });
  });
+
+ //CUT, COPY, PASTE
+ $(function () { 
+     var $p = $('#task17 p');
+     var $cloneQuote = $p.clone();
+     $p.remove();
+     $cloneQuote.insertAfter('#task17 h2');
+
+     var $moveItem = $('#task17 #one').detach();
+     $moveItem.appendTo('#task17 ul');
+  })
