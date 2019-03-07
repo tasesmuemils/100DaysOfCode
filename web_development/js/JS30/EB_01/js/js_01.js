504ca888fd1c86a2b9562cb7af472407914c5a2a
Emils Bisenieks <emils.bisenieks93@gmail.com>
2019-03-07 16:20:27 +0200

    FINAL DRUMS JS

--- web_development/js/JS30/EB_01/js/js_01.js
+++ web_development/js/JS30/EB_01/js/js_01.js
@@ -1,15 +1,22 @@
-window.addEventListener('keydown', function (e) {
+/*Created by Emils Bisenieks from Wes Bos JS30 turorials
+I will comment this out as many as possible just to note things down and just for better situation uderstanding*/
+
+function playSound(e) {
+    //Collects audio and key, with specific key code. 
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-    if (!audio) return; //if no audio, stop func
-    audio.currentTime = 0;
-    audio.play();
-    key.classList.add('playing');
-});
-
-function removeTransition(e) {
-    console.log(e);
+    if (!audio) return; //if no audio, stop function
+    audio.currentTime = 0; //rewind audio to start, because audio lenght is about 3 seconds, so it will not allow play it from begining if it is already playing
+    audio.play(); //plays audio
+    key.classList.add('playing'); //Adds extra class
 }
 
-const keys = document.querySelectorAll('.key');
-keys.forEach(key => key.addEventListener('transitioned', removeTransition));
+function removeTransition(e) {
+    if(e.propertyName !== 'transform') return; //if propertyName is something else, function stops
+    this.classList.remove('playing'); //removes added class 
+}
+
+const keys = document.querySelectorAll('.key'); //selects all keys
+keys.forEach(key => key.addEventListener('transitionend', removeTransition)); //when transition on element ends (transition: all 0.07s), it triggers a functions, which removes the class
+
+window.addEventListener('keydown', playSound);
