0ec4a5099d029ff297167edcd0dcac3cc0c06237
Emils Bisenieks <emils.bisenieks93@gmail.com>
2019-03-19 16:48:37 +0200

    js update

--- web_development/js/quiz_js/js/script.js
+++ web_development/js/quiz_js/js/script.js
@@ -4,8 +4,9 @@ button.addEventListener('click', loadQuestion);
 const button2 = document.querySelector('#button2');
 button2.addEventListener('click', function () {
     document.querySelector('.answers').style.display = 'grid';
-    loadQuestion();
+    document.querySelector('.counterSection').style.display = 'grid';
     button2.style.display = 'none';
+    loadQuestion();
 });
 
 
@@ -21,58 +22,95 @@ const checkC = document.getElementById('check_C');
 const checkD = document.getElementById('check_D');
 const radios = [checkA, checkB, checkC, checkD];
 
+const errorMessage = document.getElementById('errorMessage');
+const correctCount = document.getElementById('correct');
+const incorrectCount = document.getElementById('incorrect');
+
+
 let eventClickCount = 0;
+var correctCountValue = 0;
+var incorrectCountValue = 0;
 let i = 0;
+
+
+
+
+
 
 function loadQuestion() {
     eventClickCount++;
-    console.log('click count', eventClickCount);
+
+    //console.log('click count: ', eventClickCount);
     const xhr = new XMLHttpRequest();
     xhr.open('GET', 'js/data.json', true);
 
     xhr.onload = function () {
         if (this.status == 200) {
+
             const dataArray = JSON.parse(this.responseText);
+            //console.log("array element: ", i);
+
+            if ((eventClickCount > 1) && ((checkA.checked == false) && (checkB.checked == false) && (checkC.checked == false) && (checkD.checked == false))) {
+                return;
+            }
+
 
             var correctAnswer = dataArray[i].correct;
 
-            function quizContent() {
-                if (eventClickCount == 1) {
-                    
+           
+           
+
+            function answerCheck() {
+
+                if (eventClickCount === 1) {
+                    return;
                 }
+
+                for (let k = 0; k < radios.length; k++) {
+                    if ((radios[k].checked === true) && (radios[k].parentNode.textContent == correctAnswer)) {
+                        correctCountValue++;
+                        correctCount.textContent = correctCountValue;
+
+                    } else if ((radios[k].checked === true) && (radios[k].parentNode.textContent != correctAnswer)) {
+                        incorrectCountValue++;
+                        incorrectCount.textContent = incorrectCountValue;
+                    }
+                }
+                function uncheckAll() {
+                    for (let z = 0; z < radios.length; z++) {
+                        radios[z].checked = false;
+                    }
+                }
+                uncheckAll();
+
+            }
+
+            function nextQuestion() {
+                if (eventClickCount > 1) {
+                    i++;
+                }
+                //console.log("array element: ", i);
+                if (i == dataArray.length) {
+                    button.removeEventListener('click', loadQuestion);
+                }
+            }
+
+            function quizContent() {
                 question.textContent = dataArray[i].question;
                 answerA.textContent = dataArray[i].answer_A;
                 answerB.textContent = dataArray[i].answer_B;
                 answerC.textContent = dataArray[i].answer_C;
                 answerD.textContent = dataArray[i].answer_D;
+
             }
-            quizContent();
 
 
-            function answerCheck() {
-                for (let k = 0; k < radios.length; k++) {
-                    if ((radios[k].checked === true) && (radios[k].parentNode.textContent === correctAnswer)) {
-                        console.log(correctAnswer);
-                    } else if ((radios[k].checked === false) && (radios[k].parentNode.textContent != correctAnswer)) {
-                        //console.log("BREAK")
-                        break;
-                    } else {
-                        //console.log(' ELSE  FALSE');
-                    }
-                }
-            }
             answerCheck();
-
-
-            if (eventClickCount == 1) {
-                i = 0;
-            } else {
-                i++;
-            }
-            console.log(i);
-            if (i == dataArray.length) {
-                button.removeEventListener('click', loadQuestion);
-            }
+            
+            nextQuestion();
+            
+            quizContent();
+    
         }
     }
     xhr.send();
