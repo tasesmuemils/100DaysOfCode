4d90bf8d57d801d3ea752730e135a838af661ae1
Emils Bisenieks <emils.bisenieks93@gmail.com>
2019-03-20 12:27:04 +0200

    js quiz

--- web_development/js/quiz_js/js/script.js
+++ web_development/js/quiz_js/js/script.js
@@ -1,3 +1,7 @@
+/* JS QUIZ by EMILS BISENIEKS
+10 questions */
+
+//BUTTONS
 const button = document.querySelector('#button1');
 button.addEventListener('click', loadQuestion);
 
@@ -5,11 +9,12 @@ const button2 = document.querySelector('#button2');
 button2.addEventListener('click', function () {
     document.querySelector('.answers').style.display = 'grid';
     document.querySelector('.counterSection').style.display = 'grid';
+    document.getElementById('quizDescription').style.display = 'none';
     button2.style.display = 'none';
     loadQuestion();
 });
 
-
+//STORED VARIABLES
 const question = document.getElementById('question');
 const answerA = document.getElementById('answerA');
 const answerB = document.getElementById('answerB');
@@ -22,10 +27,10 @@ const checkC = document.getElementById('check_C');
 const checkD = document.getElementById('check_D');
 const radios = [checkA, checkB, checkC, checkD];
 
-const errorMessage = document.getElementById('errorMessage');
+const errorMessage = document.getElementById('warning');
 const correctCount = document.getElementById('correct');
 const incorrectCount = document.getElementById('incorrect');
-
+const finalScore = document.getElementById('finalScore');
 
 let eventClickCount = 0;
 var correctCountValue = 0;
@@ -33,39 +38,36 @@ var incorrectCountValue = 0;
 let i = 0;
 
 
-
-
-
-
+//FUNCTION (take data from .JSON with AJAX methods)
 function loadQuestion() {
     eventClickCount++;
 
-    //console.log('click count: ', eventClickCount);
     const xhr = new XMLHttpRequest();
     xhr.open('GET', 'js/data.json', true);
 
     xhr.onload = function () {
         if (this.status == 200) {
-
+            //Store string data from .json in array
             const dataArray = JSON.parse(this.responseText);
-            //console.log("array element: ", i);
 
+            //IF no answer is selected
             if ((eventClickCount > 1) && ((checkA.checked == false) && (checkB.checked == false) && (checkC.checked == false) && (checkD.checked == false))) {
+                errorMessage.style.display = 'block';
                 return;
+            } else {
+                errorMessage.style.display = 'none';
             }
 
-
+            //Stored correct answer
             var correctAnswer = dataArray[i].correct;
 
-           
-           
-
             function answerCheck() {
-
+                //loads first questions
                 if (eventClickCount === 1) {
                     return;
                 }
 
+                //checks answers
                 for (let k = 0; k < radios.length; k++) {
                     if ((radios[k].checked === true) && (radios[k].parentNode.textContent == correctAnswer)) {
                         correctCountValue++;
@@ -76,6 +78,8 @@ function loadQuestion() {
                         incorrectCount.textContent = incorrectCountValue;
                     }
                 }
+
+                //after every answer submit, cleans radio buttons
                 function uncheckAll() {
                     for (let z = 0; z < radios.length; z++) {
                         radios[z].checked = false;
@@ -89,12 +93,19 @@ function loadQuestion() {
                 if (eventClickCount > 1) {
                     i++;
                 }
-                //console.log("array element: ", i);
+
+                //when Quiz ends
                 if (i == dataArray.length) {
+                    document.querySelector('.answers').style.display = 'none';
+                    document.querySelector('.counterSection').style.display = 'none';
+                    question.style.display = 'none';
+                    document.getElementById('finish').style.display = 'block';
+                    finalScore.textContent = correctCountValue;
                     button.removeEventListener('click', loadQuestion);
                 }
             }
 
+            //loads quiz content
             function quizContent() {
                 question.textContent = dataArray[i].question;
                 answerA.textContent = dataArray[i].answer_A;
@@ -106,11 +117,11 @@ function loadQuestion() {
 
 
             answerCheck();
-            
+
             nextQuestion();
-            
+
             quizContent();
-    
+
         }
     }
     xhr.send();
