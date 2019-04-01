35f9eea6ddff31c5c6dfaad6b48f8e3895040333
Emils Bisenieks <emils.bisenieks93@gmail.com>
2019-04-01 16:34:47 +0300

    Update script.js

--- web_development/js/tools/js/script.js
+++ web_development/js/tools/js/script.js
@@ -16,30 +16,51 @@ function hex2rgb(hex) {
     }
 }
 
-function handleSliderUpdate(x) {
-    //Suffix will get attributes value. New way can be .dataset (this.dataset.sizing)
-    const suffix = this.getAttribute('data-sizing') || '';
-    if (this.type === 'range') {
-        document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
-        let textInput = this.previousElementSibling.lastElementChild.firstElementChild;
-        textInput.value = this.value;
-    } else {
-        document.documentElement.style.setProperty(`--${this.name}`, hex2rgb(this.value).toString() + suffix);
-        this.nextElementSibling.value = this.value
+function handleSliderUpdate() {
+    for (let i = 0; i < sliderArray.length; i++) {
+        //Suffix will get attributes value. New way can be .dataset (this.dataset.sizing)
+        const suffix = sliderArray[i].getAttribute('data-sizing') || '';
+        if (sliderArray[i].type === 'range') {
+            document.documentElement.style.setProperty(`--${sliderArray[i].name}`, sliderArray[i].value + suffix);
+            sliderArray[i].previousElementSibling.lastElementChild.firstElementChild.value = sliderArray[i].value;
+        } else {
+            document.documentElement.style.setProperty(`--${sliderArray[i].name}`, hex2rgb(sliderArray[i].value).toString());
+            sliderArray[i].nextElementSibling.value = sliderArray[i].value;
+        }
+
     }
 
-    copyCSS.textContent = `box-shadow: ${sliderArray[0].value}xp ${sliderArray[1].value}px ${sliderArray[2].value}px ${sliderArray[3].value}px rgba(${hex2rgb(sliderArray[4].value).toString()}, ${sliderArray[7].value});`
 
+    copyCSS.textContent = `box-shadow: ${sliderArray[0].value}xp ${sliderArray[1].value}px ${sliderArray[2].value}px ${sliderArray[3].value}px rgba(${hex2rgb(sliderArray[4].value).toString()}, ${sliderArray[7].value});`
 }
 
 
-
 for (let i = 0; i < sliderArray.length; i++) {
-    arrayText[i].addEventListener('input', function () {
-        if ((this.value != this.parentElement.parentElement.nextElementSibling.value) || (this.value != this.previousElementSibling.value)) {
-            (this.parentElement.parentElement.nextElementSibling.value = this.value) || (this.previousElementSibling.value = this.value);
-        }
+    sliderArray[i].addEventListener('mousemove', function () {
+        handleSliderUpdate(sliderArray[i]);
     });
-    sliderArray[i].addEventListener('change', handleSliderUpdate);
-    sliderArray[i].addEventListener('mousemove', handleSliderUpdate)
+
+    sliderArray[i].addEventListener('change', function () {
+        handleSliderUpdate(sliderArray[i]);
+    });
+
+    arrayText[i].addEventListener('change', function (e) {
+        console.log(e);
+        const sliderValueUpdate = this.parentElement.parentElement.nextElementSibling;
+        if ((this.classList.contains('pixelsInput') && this.value != sliderValueUpdate.value)) {
+            sliderValueUpdate.value = this.value;
+            handleSliderUpdate(sliderArray[i]);
+        }
+        
+
+    });
+    arrayText[i].addEventListener('change', function () {
+
+        const colorValueUpdate = this.previousElementSibling;
+        if (this.classList.contains('colorInput') && this.value != colorValueUpdate.value) {
+            colorValueUpdate.value = this.value;
+            handleSliderUpdate(sliderArray[i]);
+        }
+        
+    })
 }
