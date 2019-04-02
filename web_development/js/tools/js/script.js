00d9325de44a77b5c2f4b340f01e72bd6ee531ce
Emils Bisenieks <emils.bisenieks93@gmail.com>
2019-04-02 15:53:25 +0300

    Update script.js

--- web_development/js/tools/js/script.js
+++ web_development/js/tools/js/script.js
@@ -1,12 +1,16 @@
+/*Vanilla JS script for Shadow-Box generator
+Created by Emils Bisenieks (inspired from Wes Bos JS30 )*/
+
+//Capture necessary elements
 const sliderInputs = document.querySelectorAll(".result");
 const textInputs = document.querySelectorAll('input[type="text"]');
 const copyCSS = document.getElementById('copyCSScode');
 
-//Change NodeList to Array
+//Change NodeLists to Arrays
 const sliderArray = Array.from(sliderInputs);
 const arrayText = Array.from(textInputs);
 
-//convert hex to rgb, so opacity on shadow is available (from StackOverflow)
+//ConvertS hex to rgb, so opacity on shadow is available (from StackOverflow)
 function hex2rgb(hex) {
     r = hex.match(/^#([0-9a-f]{2})([0-9a-f]{2})([0-9a-f]{2})$/i);
     if (r) {
@@ -20,6 +24,8 @@ function handleSliderUpdate() {
     for (let i = 0; i < sliderArray.length; i++) {
         //Suffix will get attributes value. New way can be .dataset (this.dataset.sizing)
         const suffix = sliderArray[i].getAttribute('data-sizing') || '';
+
+        //If input type === 'range', add suffix, else = no suffix added
         if (sliderArray[i].type === 'range') {
             document.documentElement.style.setProperty(`--${sliderArray[i].name}`, sliderArray[i].value + suffix);
             sliderArray[i].previousElementSibling.lastElementChild.firstElementChild.value = sliderArray[i].value;
@@ -27,40 +33,40 @@ function handleSliderUpdate() {
             document.documentElement.style.setProperty(`--${sliderArray[i].name}`, hex2rgb(sliderArray[i].value).toString());
             sliderArray[i].nextElementSibling.value = sliderArray[i].value;
         }
-
     }
 
-
+    //Copy CSS text for user
     copyCSS.textContent = `box-shadow: ${sliderArray[0].value}xp ${sliderArray[1].value}px ${sliderArray[2].value}px ${sliderArray[3].value}px rgba(${hex2rgb(sliderArray[4].value).toString()}, ${sliderArray[7].value});`
 }
 
-
+//ADDS EVENT TO ALL INPUTS
 for (let i = 0; i < sliderArray.length; i++) {
+
+    //Fires function if mouse move over slider
     sliderArray[i].addEventListener('mousemove', function () {
-        handleSliderUpdate(sliderArray[i]);
+        handleSliderUpdate();
     });
 
+    //On slider change
     sliderArray[i].addEventListener('change', function () {
-        handleSliderUpdate(sliderArray[i]);
+        handleSliderUpdate();
     });
 
-    arrayText[i].addEventListener('change', function (e) {
-        console.log(e);
+    //For slider values
+    arrayText[i].addEventListener('change', function () {
         const sliderValueUpdate = this.parentElement.parentElement.nextElementSibling;
         if ((this.classList.contains('pixelsInput') && this.value != sliderValueUpdate.value)) {
             sliderValueUpdate.value = this.value;
-            handleSliderUpdate(sliderArray[i]);
+            handleSliderUpdate();
         }
-        
-
     });
-    arrayText[i].addEventListener('change', function () {
 
+    //For colors
+    arrayText[i].addEventListener('change', function () {
         const colorValueUpdate = this.previousElementSibling;
         if (this.classList.contains('colorInput') && this.value != colorValueUpdate.value) {
             colorValueUpdate.value = this.value;
-            handleSliderUpdate(sliderArray[i]);
+            handleSliderUpdate();
         }
-        
     })
 }
