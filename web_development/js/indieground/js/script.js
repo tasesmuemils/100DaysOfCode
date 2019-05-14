b6a0ab80e434ba815f73b12fead7bcd12e2f605d
EMILS <emils.bisenieks93@gmail.com>
2019-05-14 23:57:01 +0300

    fail

--- web_development/js/indieground/js/script.js
+++ web_development/js/indieground/js/script.js
@@ -1,18 +1,22 @@
-//Parallax background efect
-const parallaxBg = document.querySelectorAll('.parallaxBg');
+// //Parallax background efect
+const parallaxBgNodeList = document.querySelectorAll('.parallaxBg');
+const parallaxBgArray = Array.from(parallaxBgNodeList);
 
 function parallaxEffect(e) {
     let scrolled = window.pageYOffset;
-    //console.log(scrolled);
-    parallaxBg.forEach(
-        bg => {
-            if ((scrolled / 0.63) >= bg.offsetTop) {
-                bg.style.backgroundPositionY = (scrolled * 0.64 + 'px');
-            }
-            
-            console.log(scrolled, bg, bg.offsetTop, bg.style.backgroundPositionY);
-        }
-    );
+    //parallaxBgArray[0].style.backgroundPositionY =  (scrolled * 0.7) + 'px';
 }
 
 window.addEventListener('scroll', parallaxEffect);
+
+// parallaxBg.forEach(
+//     bg => {
+//         if ((scrolled / 0.63) >= bg.offsetTop) {
+//             bg.style.backgroundPositionY = (scrolled * 0.64 + 'px');
+//         }
+
+//         console.log(scrolled, bg, bg.offsetTop, bg.style.backgroundPositionY);
+//     }
+// );
+
+//parallaxBgArray[i].style.backgroundPositionY = ((scrolled * 1.2) + 'px');
