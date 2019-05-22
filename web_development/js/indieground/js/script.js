84f595fcd2edf74cd3ac477ce397c5a1ce523e30
Emils Bisenieks <emils.bisenieks93@gmail.com>
2019-05-22 16:38:21 +0300

    Update script.js

--- web_development/js/indieground/js/script.js
+++ web_development/js/indieground/js/script.js
@@ -1,74 +1,3 @@
-// //Parallax background efect (for one thin at the moment)
-const parallaxBgNodeList = document.querySelectorAll('.parallaxBg');
-const parallaxBgArray = Array.from(parallaxBgNodeList);
-
-function parallaxEffect(e) {
-    const scrolled = window.pageYOffset;
-    for (let i = 0; i < parallaxBgArray.length; i++) {
-        const parallaxDivTop = parallaxBgArray[i].parentElement.offsetTop;
-        const rate = (scrolled * 0.5 - (parallaxDivTop / 2) + 300);
-        parallaxBgArray[i].style.transform = 'translateY(' + rate + 'px)';
-    }
-
-}
-
-window.addEventListener('scroll', parallaxEffect);
-
-
-//Images carousel
-const sliderWrapper = document.getElementById('slider');
-const carouselContainer = document.getElementById('img-carousel');
-const carouselImagesNodeList = carouselContainer.querySelectorAll('img');
-const carouselImages = Array.from(carouselImagesNodeList);
-const carouselImagesCount = carouselImages.length;
-
-const sliderWrapperHeight = carouselImages[carouselImages.length - 1].height + 'px';
-const sliderWrapperWidth = carouselImages[carouselImages.length - 1].width + 'px';
-
-const carouselContainerWidth = carouselImagesCount * carouselImages[0].width;
-
-
-
-sliderWrapper.style.width = sliderWrapperWidth;
-sliderWrapper.style.height = sliderWrapperHeight;
-
-carouselContainer.style.width = carouselContainerWidth + 12 + 'px';
-
-carouselContainer.insertBefore(carouselContainer.lastElementChild, carouselContainer.firstElementChild);
-carouselContainer.style.marginLeft = -carouselImages[carouselImagesCount - 1].width + 'px';
-
-
-window.setInterval(carouselAnimation, 1000);
-
-function carouselAnimation() {
-    
-    carouselContainer.classList.add('imgCarouselAnimation');
-    //carouselContainer.style.transform = 'translateX(' + '-' + sliderWrapperWidth + ')';
-    carouselContainer.insertBefore(carouselContainer.lastElementChild, carouselContainer.firstElementChild);
-
-    console.log(carouselContainer.style.transform);
-
-    //console.log(intervalCount);
-    // carouselContainer.classList.add('imgCarouselAnimation');
-    // carouselContainer.style.transform = 'translateX(' + '-' + sliderWrapperWidth + ')';
-
-    //console.log(carouselImages);
-
-    //carouselContainer.style.transform = 'translateX(0px)';
-    //carouselContainer.classList.remove('imgCarouselAnimation');
-    // carouselContainer.style.marginLeft = - carouselImages[carouselImages.length - 1].width * 2 + 'px';
-    //console.log(carouselContainer.style.transform);
-    
-
-}
-
-
-
-
-
-
-
-
 //Toogle navigation button 
 const toogleButton = document.querySelector('.toogle-button');
 const closeToogle = document.getElementById('close-toogle');
@@ -87,3 +16,60 @@ function closeMenu() {
 
 toogleButton.addEventListener('click', toogleMenu);
 closeToogle.addEventListener('click', closeMenu);
+
+
+
+//Parallax background efect (for one thin at the moment)
+const parallaxBgNodeList = document.querySelectorAll('.parallaxBg');
+const parallaxBgArray = Array.from(parallaxBgNodeList);
+
+function parallaxEffect(e) {
+    const scrolled = window.pageYOffset;
+    for (let i = 0; i < parallaxBgArray.length; i++) {
+        const parallaxDivTop = parallaxBgArray[i].parentElement.offsetTop;
+        const rate = (scrolled * 0.5 - (parallaxDivTop / 2) + 300);
+        parallaxBgArray[i].style.transform = 'translateY(' + rate + 'px)';
+    }
+
+}
+
+window.addEventListener('scroll', parallaxEffect);
+
+
+
+//Images carousel
+const sliderWrapperNodeList = document.querySelectorAll('.slider');
+const sliderWrapper = Array.from(sliderWrapperNodeList);
+
+for (let i = 0; i < sliderWrapper.length; i++) {
+    const carouselContainer = sliderWrapper[i].firstElementChild;
+    const carouselImagesNodeList = carouselContainer.querySelectorAll('img');
+    const carouselImages = Array.from(carouselImagesNodeList);
+    const carouselImagesCount = carouselImages.length;
+
+    const sliderWrapperHeight = carouselImages[carouselImagesCount - 1].height + 'px';
+    const sliderWrapperWidth = carouselImages[carouselImagesCount - 1].width + 'px';
+
+    sliderWrapper[i].style.width = sliderWrapperWidth;
+    sliderWrapper[i].style.height = sliderWrapperHeight;
+
+    const carouselContainerWidth = carouselImagesCount * carouselImages[0].width;
+
+    carouselContainer.style.width = carouselContainerWidth + 12 + 'px';
+    carouselContainer.style.marginLeft = -carouselImages[carouselImagesCount - 1].width + 'px';
+
+    var animationInterval = setInterval(carouselAnimation, 3000);
+
+    function carouselAnimation() {
+        carouselContainer.classList.add('imgCarouselAnimation');
+        carouselContainer.style.transform = 'translateX(' + '-' + sliderWrapperWidth + ')';
+    }
+
+    carouselContainer.addEventListener('transitionend', function (e) {
+        carouselContainer.insertBefore(carouselContainer.firstElementChild, carouselContainer.lastElementChild.nextSibling);
+        carouselContainer.style.transform = '';
+        carouselContainer.classList.remove('imgCarouselAnimation');
+    })
+}
+
+
