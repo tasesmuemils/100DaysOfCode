98f12b35262acb9ef30e9dd3cf763d1ddecbd1f3
Emils Bisenieks <emils.bisenieks93@gmail.com>
2019-11-22 15:16:21 +0200

    Add files via upload

--- web_development/kd_app/scripts/script.js
+++ web_development/kd_app/scripts/script.js
@@ -3,6 +3,7 @@
 var url = "https://tasesmuemils.github.io/json_test/kg_data.json";
 
 fetch(url)
-    .then(response => console.log(response));
+    .then(response => response.json())
+    .then(data => console.log(data));
 
-/* TEST */
+
