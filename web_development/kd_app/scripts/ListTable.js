c75f1e901f1137ed2593a44f60accd65c8ea727a
EMILS <emils.bisenieks93@gmail.com>
2019-12-09 23:50:27 +0200

    back

---
+++ web_development/kd_app/scripts/ListTable.js
@@ -0,0 +1,10 @@
+const dataTable = document.getElementById("group-table");
+const tableBody = document.createElement("tbody");
+const tableBodyRow = document.createElement("tr");
+
+tableBody.appendChild(tableBodyRow);
+dataTable.appendChild(tableBody);
+
+
+
+console.log(dataTable);
--- web_development/kd_app/scripts/script.js
+++ web_development/kd_app/scripts/script.js
@@ -1,9 +1,31 @@
+const dataUrl = "https://tasesmuemils.github.io/json_test/kg_data.json";
 
+function downloadData() {
 
-var url = "https://tasesmuemils.github.io/json_test/kg_data.json";
+    fetch(dataUrl)
+        .then(response => response.json());
+        console.log(response);
+}
 
-fetch(url)
-    .then(response => response.json())
-    .then(data => console.log(data));
+downloadData();
 
+// data.forEach(arrayItem => {
+            //     console.log(arrayItem);
+            //     // for (let i = 0; i < arrayItem.length; i++) {
+            //     //     console.log(i);
+            //     //     // const tableCell = document.createElement("td");
+            //     //     // tableCell.textContent = i;
+                    
+            //     //     // const tableBodyRow = document.createElement("tr");
+            //     //     // tableBodyRow.appendChild(tableCell);
 
+            //     //     // const dataTable = document.getElementById("group-table");
+            //     //     // const tableBody = document.createElement("tbody");
+                    
+        
+        
+
+            //     //     // tableBody.appendChild(tableBodyRow); 
+            //     //     // dataTable.appendChild(tableBody);
+            //     // }
+            // }) 
