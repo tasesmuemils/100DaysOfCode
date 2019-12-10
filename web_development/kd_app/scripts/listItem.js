7d5ed9da610c785b16e19746e24696bf39fdc972
EMILS <emils.bisenieks93@gmail.com>
2019-12-10 16:37:42 +0200

    data load

--- web_development/kd_app/scripts/listItem.js
+++ web_development/kd_app/scripts/listItem.js
@@ -1,3 +1,32 @@
-class ListItem {
-    
+export function downloadData() {
+
+    const dataUrl = "https://tasesmuemils.github.io/json_test/kg_data.json";
+    fetch(dataUrl)
+        .then(response => response.json())
+        .then(data => loadTable(data))
+
+    function loadTable(dataArray) {
+        console.log(dataArray);
+        for (let i = 0; i < dataArray.length; i++) {
+            const dataObject = dataArray[i];
+            const valuesArray = Object.values(dataObject);
+
+            const tableBodyRow = document.createElement("tr");
+            const tableBody = document.createElement("tbody");
+
+            for (let i = 0; i < 5; i++) {
+                const tableCell = document.createElement("td");
+                tableCell.textContent = valuesArray[i];
+
+                tableBodyRow.appendChild(tableCell);
+
+            }
+
+            const dataTable = document.getElementById("group-table");
+            tableBody.appendChild(tableBodyRow);
+            dataTable.appendChild(tableBody);
+
+
+        }
+    }
 }
--- web_development/kd_app/scripts/script.js
+++ web_development/kd_app/scripts/script.js
@@ -1,35 +1,3 @@
-const dataUrl = "https://tasesmuemils.github.io/json_test/kg_data.json";
-
-function downloadData() {
-    fetch(dataUrl)
-        .then(response => response.json())
-        .then(data => loadTable(data))
-}
+import { downloadData } from "./listItem.js";
 
 downloadData();
-
-function loadTable(dataArray) {
-    console.log(dataArray);
-    for (let i = 0; i < dataArray.length; i++) {
-        const dataObject = dataArray[i];
-        const valuesArray = Object.values(dataObject);
-
-        const tableBodyRow = document.createElement("tr");
-        const tableBody = document.createElement("tbody");
-
-        for (let i = 0; i < 5; i++) {
-            const tableCell = document.createElement("td");
-            tableCell.textContent = valuesArray[i];
-            
-            tableBodyRow.appendChild(tableCell);
-            
-        }
-       
-
-        const dataTable = document.getElementById("group-table");
-        tableBody.appendChild(tableBodyRow); 
-        dataTable.appendChild(tableBody);
-       
-      
-    }
-}
